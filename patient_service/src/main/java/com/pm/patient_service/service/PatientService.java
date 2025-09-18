e020ab21e4f7061141d1147e9331a0a7619f891c
E66@lution44 <issa.berni03@gmail.com>
2025-09-18 18:04:26 +0200

    feat: add PatientMapper and complete service layer mapping
    - Introduced PatientMapper to convert Patient entities into PatientResponseDTOs
    - Updated PatientService.getPatients() to use PatientMapper with stream mapping
    - Ensured DTO-based responses instead of exposing entity directly

--- patient_service/src/main/java/com/pm/patient_service/service/PatientService.java
+++ patient_service/src/main/java/com/pm/patient_service/service/PatientService.java
@@ -1,10 +1,12 @@
 package com.pm.patient_service.service;
 
 import com.pm.patient_service.dto.PatientResponseDTO;
+import com.pm.patient_service.mapper.PatientMapper;
 import com.pm.patient_service.model.Patient;
 import com.pm.patient_service.repository.PatientRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -17,7 +19,7 @@ public class PatientService {
 
     public List<PatientResponseDTO> getPatients(){
         List<Patient> patients = patientRepository.findAll();
-
-
+        return patients.stream().map(
+                PatientMapper::toDTO).toList();
     }
 }
