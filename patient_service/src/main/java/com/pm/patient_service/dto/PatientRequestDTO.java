bc2daecf989330974ebbef53c0b14e9118cb2793
E66@lution44 <issa.berni03@gmail.com>
2025-09-19 10:28:17 +0200

    feat(dto): add PatientRequestDTO with validation constraints
    - Introduced PatientRequestDTO to handle patient creation requests
    - Added validation annotations for name, email, address, birthDate, and registeredDate
    - Ensures input data integrity and enforces required fields

---
+++ patient_service/src/main/java/com/pm/patient_service/dto/PatientRequestDTO.java
@@ -0,0 +1,68 @@
+package com.pm.patient_service.dto;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+
+public class PatientRequestDTO {
+    @NotBlank(message = "Name is required")
+    @Size(max = 100, message = "Name must be at most 100 characters long")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "address is required")
+    private String address;
+
+    @NotNull(message = "date of birth is required")
+    private LocalDate birthDate;
+
+    @NotNull(message="Registered date is required")
+    private LocalDate registeredDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public LocalDate getRegisteredDate() {
+        return registeredDate;
+    }
+
+    public void setRegisteredDate(LocalDate registeredDate) {
+        this.registeredDate = registeredDate;
+    }
+}
